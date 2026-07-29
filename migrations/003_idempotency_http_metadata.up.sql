59339b156020cb529fcdddf49963330553770384
Prince (Kofi Frimpong) Amissah <princeamissah0@gmail.com>
2026-07-29 09:21:30 +0000

    Fix idempotency metadata trigger

--- migrations/003_idempotency_http_metadata.up.sql
+++ migrations/003_idempotency_http_metadata.up.sql
@@ -5,15 +5,16 @@ RETURNS trigger
 LANGUAGE plpgsql
 AS $$
 BEGIN
-  NEW.response_status := CASE
-    WHEN NEW.scope IN ('create-mandate', 'create-approval', 'issue-receipt') THEN 201
-    WHEN NEW.scope = 'authorize'
-      OR NEW.scope LIKE 'revoke-mandate:%'
-      OR NEW.scope LIKE 'decide-approval:%' THEN 200
-    ELSE
-      RAISE EXCEPTION 'unknown idempotency scope %', NEW.scope
-        USING ERRCODE = '22023';
-  END CASE;
+  IF NEW.scope IN ('create-mandate', 'create-approval', 'issue-receipt') THEN
+    NEW.response_status := 201;
+  ELSIF NEW.scope = 'authorize'
+    OR NEW.scope LIKE 'revoke-mandate:%'
+    OR NEW.scope LIKE 'decide-approval:%' THEN
+    NEW.response_status := 200;
+  ELSE
+    RAISE EXCEPTION 'unknown idempotency scope %', NEW.scope
+      USING ERRCODE = '22023';
+  END IF;
 
   NEW.response_headers := jsonb_build_object(
     'content-type', 'application/json; charset=utf-8',
