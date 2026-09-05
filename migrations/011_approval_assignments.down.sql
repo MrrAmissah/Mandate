65351c9a79cef5a59cb8d93fd3a1ae5ada289172
Prince (Kofi Frimpong) Amissah <princeamissah0@gmail.com>
2026-09-05 22:02:11 +0000

    Remove approval authority validation triggers on rollback

--- migrations/011_approval_assignments.down.sql
+++ migrations/011_approval_assignments.down.sql
@@ -5,6 +5,16 @@ DROP FUNCTION IF EXISTS mandate.validate_approval_operational_transition();
 DROP TRIGGER IF EXISTS approvals_evidence_immutability_guard ON mandate.approvals;
 DROP FUNCTION IF EXISTS mandate.guard_approval_evidence_immutability();
 DROP TRIGGER IF EXISTS approval_assignment_eligibility_immutable ON mandate.approval_assignment_eligibility;
+DROP TRIGGER IF EXISTS approval_assignment_eligibility_source_guard ON mandate.approval_assignment_eligibility;
+DROP FUNCTION IF EXISTS mandate.validate_approval_assignment_eligibility();
+DROP TRIGGER IF EXISTS approval_assignments_source_guard ON mandate.approval_assignments;
+DROP FUNCTION IF EXISTS mandate.validate_approval_assignment_source();
+DROP TRIGGER IF EXISTS approval_assignments_history_guard ON mandate.approval_assignments;
+DROP TRIGGER IF EXISTS approver_group_memberships_history_guard ON mandate.approver_group_memberships;
+DROP TRIGGER IF EXISTS approver_groups_history_guard ON mandate.approver_groups;
+DROP TRIGGER IF EXISTS approver_credential_bindings_history_guard ON mandate.approver_credential_bindings;
+DROP TRIGGER IF EXISTS approver_identities_history_guard ON mandate.approver_identities;
+DROP FUNCTION IF EXISTS mandate.guard_approval_authority_history();
 
 ALTER TABLE mandate.approvals
   DROP CONSTRAINT IF EXISTS approvals_cancelled_by_credential_fk,
