af8d1ad08e007481d9bc85da13b6d7589f449a34
Prince (Kofi Frimpong) Amissah <princeamissah0@gmail.com>
2026-09-05 23:18:23 +0000

    Record approval inbox migration rollback

--- migrations/013_approval_inbox_indexes.down.sql
+++ migrations/013_approval_inbox_indexes.down.sql
@@ -3,4 +3,7 @@ BEGIN;
 DROP INDEX IF EXISTS mandate.approvals_pending_inbox_order_idx;
 DROP INDEX IF EXISTS mandate.approval_assignment_eligibility_inbox_idx;
 
+DELETE FROM mandate.schema_migrations
+WHERE version = '013_approval_inbox_indexes';
+
 COMMIT;
